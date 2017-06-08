5bcc267a7a290629cbaeeb9a2788503a63b98edf
Eunice Wanjiru <eunice.wanjiru@andela.com>
2017-06-08 16:01:44 +0300

    configure sequezile and create entry file

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+process.env.SECRET_KEY = 'superSecretString';
+const app = express();
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+require('./server/routes/documents')(app);
+require('./server/routes/users')(app);
+app.get('/', (req, res) => res.status(200).send({
+  message: 'Welcome to the Document Management System.',
+}));
+
+module.exports = app;

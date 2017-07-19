557a74f950864fa62fa27e696d43daf46c16a04c
Eunice Wanjiru <eunice.wanjiru@andela.com>
2017-07-19 16:12:56 +0300

    [feature] correct link to use Link

--- client/components/common/SubHeader.js
+++ client/components/common/SubHeader.js
@@ -3,10 +3,10 @@ import { Link } from 'react-router';
 
 const SubHeader = props => (
   <ul className="nav nav-pills" >
-    <li role="presentation"><a href="/edocx/documents">All Documents</a></li>
-    <li role="presentation"><a href="/edocx/documents/mydocuments" name="mine">My Documents</a></li>
-    <li role="presentation"><a href="/edocx/documents/roledocuments">{props.role} Documents</a></li>
-    <li role="presentation"><a href="/edocx/documents/newdocument" name="new">New Document</a></li>
+    <li role="presentation"><Link to="/edocx/documents">All Documents</Link></li>
+    <li role="presentation"><Link to="/edocx/documents/mydocuments" name="mine">My Documents</Link></li>
+    <li role="presentation"><Link to="/edocx/documents/roledocuments">{props.role} Documents</Link></li>
+    <li role="presentation"><Link to="/edocx/documents/newdocument" name="new">New Document</Link></li>
   </ul>
 );
 
